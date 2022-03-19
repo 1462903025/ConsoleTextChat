fa00daae1d739f749a8106a6cbf0b8c9776b53d8
GraczBezNicku <58118380+GraczBezNicku@users.noreply.github.com>
2022-03-19 14:52:44 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using Exiled.Events;
+
+namespace ConsoleTextChat
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Singleton;
+        public EventHandler eventHandler;
+
+        public List<string> mutedPlayers = new List<string>();
+
+        public override string Name => "ConsoleTextChat";
+        public override string Author => "GBN";
+        public override Version Version => new Version(1, 0, 1);
+        public override Version RequiredExiledVersion => new Version(5, 0, 0);
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            eventHandler = new EventHandler();
+
+            Exiled.Events.Handlers.Player.Verified += eventHandler.OnPlayerVerified;
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Verified -= eventHandler.OnPlayerVerified;
+
+            eventHandler = null;
+
+            base.OnDisabled();
+        }
+    }
+}
